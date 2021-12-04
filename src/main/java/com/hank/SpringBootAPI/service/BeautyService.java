f152b3e5173310600fa88392300fc6170cc452ed
Hank Huang <hank21510@gmail>
2021-12-04 15:41:42 +0800

    @TEST

--- src/main/java/com/hank/SpringBootAPI/service/BeautyService.java
+++ src/main/java/com/hank/SpringBootAPI/service/BeautyService.java
@@ -2,7 +2,6 @@ package com.hank.SpringBootAPI.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -77,4 +76,31 @@ public class BeautyService {
 //		rtnData.put("data", data);
 //		return rtnData;
 //	}
+	
+	public WebDriver launchBrowser() {
+		String driverPath = "";
+		if (getOS().equals(OS.WINDOWS)) {
+			driverPath = "agent//chromedriver.exe";
+			System.setProperty("webdriver.chrome.driver", driverPath);
+		}
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("window-size=1200x600");
+		if (getOS().equals(OS.LINUX)) {
+			try { // GOOGLE_CHROME_SHIM GOOGLE_CHROME_BIN
+				String binaryPath = EnvironmentUtils.getProcEnvironment().get("GOOGLE_CHROME_SHIM");
+				System.out.println("Path: " + binaryPath);
+				options.setBinary(binaryPath);
+				options.addArguments("--disable-gpu");
+				options.addArguments("--no-sandbox");
+			} catch (Exception e) {
+
+			}
+		}
+
+		WebDriver driver = new ChromeDriver(options);
+
+		return driver;
+	}
 }
